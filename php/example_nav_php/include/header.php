84542b7cbeed5f4f325fb4c3d68f949f2568aa75
leoduchateau <32895315+leoduchateau@users.noreply.github.com>
2017-10-23 12:27:58 +0200

    ok

--- php/example_nav_php/include/header.php
+++
@@ -1,8 +0,0 @@
-<!-- header -->
-<div class="container">
-	<div class="row" style ="background-color: red; height: 70px">
-		<div class="col">
-			<a href="/?page=about">A propos</a> | <a href="/?page=xxx">Cursus</a> | FAQ | <a href="/?page=contact">Contact</a>
-		</div>
-	</div>
-</div>
--- php/header.php
+++
@@ -1,8 +0,0 @@
-<!--header-->
-	<div class="container" style="background-color: red ; height: 70px">
-		<div class="row">
-			<div class="col">
-				A propos | Cursus | Contact
-			</div>
-		</div>
-	</div>
