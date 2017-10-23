5039fe52a6b2edfa148adf4d15555b60241ce798
NicolasBondesan <32895264+NicolasBondesan@users.noreply.github.com>
2017-10-23 14:30:21 +0200

    uyu

--- php/page/contact.php
+++ php/page/contact.php
@@ -1,71 +1,6 @@
-<html lang="fr">
-
-	<head>
-
-	
-
-		<title>formulaire</title>
-		<meta charset="utf-8">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-		<link rel="stylesheet" href="contact.css" >
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
-		
-
-		
-
-	</head>
-	
-	
-	<body>
-	<header class="">
-	
-	     <nav class="navbar navbar-expand-md">
-         
-            <a href="index leo.html" ><img src="image/monkeys.png" height="90" alt="image"> </a>
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav11" aria-controls="navbarNav11" aria-expanded="false" aria-label="Toggle navigation">
-               <span class="navbar-toggler-icon"></span>
-				</button>
-         
-				<div class="collapse navbar-collapse" id="navbarNav11">
-               <ul class="navbar-nav ml-auto">
-                 <li class="nav-item active">
-                   <a class="nav-link" href="">Formation<span class="sr-only">(current)</span></a>
-                 </li>
-                 <li class="nav-item">
-                   <a class="nav-link" href=""></a>
-                 </li>
-                 <li class="nav-item">
-                   <a class="nav-link" href="">Projet</a>
-                 </li>
-                 <li class="nav-item">
-                   <a class="nav-link" href="">contact</a>
-                 </li>
-                 <li class="nav-item"> 
-                
-                 </li>
-               </ul>
-         
-            </div>
-			
-	        
-	        
-         </nav>
-		 
-    </header>
-	
-	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2783.3917952801576!2d4.906756600000009!3d45.7633378!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f4c05257aa8e23%3A0x3dbb168b025f0!2s21+Impasse+Fr%C3%A9d%C3%A9ric+Fays%2C+69100+Villeurbanne!5e0!3m2!1sen!2sfr!4v1508402171492" width="100%" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
-	
-	
-	<div class="container-fluid bandeaujaune"> viens boire un café
-	</div>
-	
-	
-         
-       
-		
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2783.3917952801576!2d4.906756600000009!3d45.7633378!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f4c05257aa8e23%3A0x3dbb168b025f0!2s21+Impasse+Fr%C3%A9d%C3%A9ric+Fays%2C+69100+Villeurbanne!5e0!3m2!1sen!2sfr!4v1508402171492" width="100%" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+<div class="container-fluid bandeaujaune"> viens boire un café
+</div>
         <div class="col-12 col-md-6 ml-auto pt-5 pt-md-0">
           <form>
             <div class="row">
@@ -100,8 +35,3 @@
             </div>
           </form>
         </div>
-
-  
-	
-	</body>
-	</html>
