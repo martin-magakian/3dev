a20bd21b29802d31ddc13ae83b1537a9cf3d067d
KellyMazard <32895281+KellyMazard@users.noreply.github.com>
2017-10-25 16:50:10 +0200

    slider responsive

--- php/page/home.php
+++ php/page/home.php
@@ -92,17 +92,18 @@
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
 	<ol class="carousel-indicators">
 		<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-		<li data-target="#carouselExampleIndicators" data-slide-to="1" class="active"></li>
+		<!--<li data-target="#carouselExampleIndicators" data-slide-to="1" class="active"></li>-->
+		<li data-target="#carouselExampleIndicators" data-slide-to="2" class="active"></li>
 	</ol>
-	<div class="carousel-inner" style="background-color : #f2d500; height : 25em ; ">
-		<div class="carousel-item active">
+	<div class="carousel-inner" style="background-color : #f2d500;">
+		<!--<div class="carousel-item active">
 			<div class="d-block w-100" alt="First slide" >
-				<div class="container-fluid">	
+				<div class="container">	
 					<div class="row">
-						<div class="col popo">
+						<div class="col-lg-6">
 							<img src="image/slide/popo.png" style="height : 75% ;" class="img-fluid popo" alt="Responsive image">
 						</div>
-						<div class="col Texteslide">
+						<div class="col-sm-12 col-lg-6 texteslide">
 							<div class="fille"><h4>Comment se déroule un projet ?</h4>
 							<br /><br />
 							<p>Nous analysons les besoins du client pour réaliser une proposition technique. Si cette proposition est acceptée, on organise une réunion Kick-off pour  lancer le projet. On commence par travailler l’ergonomie, puis la création graphique, et enfin le développement et l’intégration graphique. Puis nous réalisons une série de tests sous différents environnements (différents navigateurs, tailles d’écrans, différents appareils comme les tablettes et mobiles) . Pour finir nous présentons le projet au client qui valide ou qui demande éventuellement des retouches.</p>
@@ -111,34 +112,35 @@
 					</div>
 				</div>
 			</div>	
-		</div>
-		<div class="carousel-item">
+		</div>-->
+		<div class="carousel-item active">
 			<div class="d-block w-100" alt="second slide" >
-				<div class="container-fluid">	
+				<div class="container">	
 					<div class="row">
-						<div class="col popo">
-							<img src="image/slide/slydephoto12.png" style="height : 75% ;" class="img-fluid popo" alt="Responsive image">
+						<div class="col">
+							<img src="image/slide/slydephoto12.png" class="slider-img" class="img-fluid popo" alt="Responsive image">
 						</div>
-						<div class="col Texteslide">
-							<div class="mec"><h4>Quelle est la plus grosse difficulté dans ce métier ?</h4>
-											<br /><br />
-							                <p>Il faut être patient et serein. Parfois on est face à une difficulté et 
-											les choses ne fonctionnent pas alors qu’on est sûr qu’on a tout bien fait. Il faut 
-											savoir prendre du recul sur son travail.</p>
+						<div class="col-sm-12 col-lg-6 texteslide vcenter">
+							<div>
+								<h4>Quelle est la plus grosse difficulté dans ce métier ?</h4>
+								<br /><br />
+							    <p>Il faut être patient et serein. Parfois on est face à une difficulté et 
+									les choses ne fonctionnent pas alors qu’on est sûr qu’on a tout bien fait. Il faut 
+									savoir prendre du recul sur son travail.</p>
 							</div>
-						</div>	
+						</div>
 					</div>
 				</div>
 			</div>	
 		</div>
-		<div class="carousel-item">
+		<!--<div class="carousel-item">
 			<div class="d-block w-100" alt="third slide">
-				<div class="container-fluid">	
+				<div class="container">	
 					<div class="row">
-						<div class="col plp">
+						<div class="col ">
 							<img src="image/slide/slydephoto13.png" style="height : 75% ;" class="img-fluid popo" alt="Responsive image">
 						</div>
-						<div class="col Texteslide">
+						<div class="col-sm-12 col-lg-6  texteslide">
 							<div class="etud"> <h4>comment voyez-vous ce métier dans quelques années ?</h4>
 												<br /><br />
 												<p>A mon avis le métier de développeur va se démocratiser, il y a besoin de développeurs dans tout 
@@ -149,7 +151,7 @@
 					</div>
 				</div>
 			</div>	
-		</div>
+		</div>-->
 	
 	<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
 		<span class="carousel-control-prev-icon" aria-hidden="true"></span>
