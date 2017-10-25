8790d8fef2f72a4f4201b95c649a8e2e670da58c
KellyMazard <32895281+KellyMazard@users.noreply.github.com>
2017-10-25 14:41:33 +0200

    slyde avec vraie txt

--- php/page/home.php
+++ php/page/home.php
@@ -91,8 +91,10 @@
 							<img src="image/slide/popo.png" style="height : 75% ;" class="img-fluid popo" alt="Responsive image">
 						</div>
 						<div class="col Texteslide">
-							<p class="kelly" >Emodicil inimil ipicid que nosa dolupta ernatibea quae vera voluptatur, sa venet omnim eaquas delenecte exceprehendi dolest ea am inum, nonet lautatur? Orrum reperes tibuscime sunte sit facearchil im ist ea sinctemporem is doluptatem in plab int, odionsequam num hillo te ex et ipiendita conseque nimoloris est alitibera aspersp edignimus dero eatiore molupta parum quaeri acipiendis eum digendus et iusda que pedis inciist, ut eaquibusa dolut in pa volupta quidescimpor si volorem ra voluptat.
-							Optae nos pro cupta volupta dolores eriatus ideliae etur, esti quatis vend</p>
+							<p class="fille" >  Comment se déroule un projet ?
+							<br /><br />
+							Nous analysons les besoins du client pour réaliser une proposition technique. Si cette proposition est acceptée, on organise une réunion 			Kick-off pour  lancer le projet. On commence par travailler l’ergonomie, puis la création graphique, et enfin le développement et l’intégration graphique. Puis nous réalisons une série de tests sous différents environnements (différents navigateurs, tailles d’écrans, différents appareils comme les tablettes et mobiles) . Pour finir nous présentons le projet au client qui valide ou qui demande éventuellement des retouches.
+							</p>
 						</div>
 					</div>
 				</div>
@@ -106,14 +108,23 @@
 							<img src="image/slide/slydephoto12.png" style="height : 75% ;" class="img-fluid popo" alt="Responsive image">
 						</div>
 						<div class="col Texteslide">
-							<p class="kelly" >Emodicil inimil ipicid que nosa dolupta ernatibea quae vera voluptatur, sa venet omnim eaquas delenecte exceprehendi dolest ea am inum, nonet lautatur? Orrum reperes tibuscime sunte sit facearchil im ist ea sinctemporem is doluptatem in plab int, odionsequam num hillo te ex et ipiendita conseque nimoloris est alitibera aspersp edignimus dero eatiore molupta parum quaeri acipiendis eum digendus et iusda que pedis inciist, ut eaquibusa dolut in pa volupta quidescimpor si volorem ra voluptat.
-							Optae nos pro cupta volupta dolores eriatus ideliae etur, esti quatis vend</p>
+							<p class="mec" >Quelle est la plus grosse difficulté dans ce métier ?
+											<br /><br />
+							                  Il faut être patient et serein. Parfois on est face à une difficulté et 
+											  les choses ne fonctionnent pas alors qu’on est sûr qu’on a tout bien fait. Il faut 
+											  savoir prendre du recul sur son travail.
+											  <br /><br />
+											  Pouvez-vous décrire en quelques mots ce qu’est un développeur web ?
+											  <br /><br />
+											  C’est une personne qui traduit techniquement les besoins fonctionnels 
+											  d’un client dans un language que le serveur comprend. En fait le développeur 
+											  web est un technicien. </p>
 						</div>
 					</div>
 				</div>
 			</div>	
 		</div>
-				<div class="carousel-item">
+		<!--<div class="carousel-item">
 			<div class="d-block w-100" alt="third slide">
 				<div class="container-fluid">	
 					<div class="row">
@@ -121,13 +132,24 @@
 							<img src="image/slide/slydephoto13.png" style="height : 75% ;" class="img-fluid popo" alt="Responsive image">
 						</div>
 						<div class="col Texteslide">
-							<p class="kelly" >Emodicil inimil ipicid que nosa dolupta ernatibea quae vera voluptatur, sa venet omnim eaquas delenecte exceprehendi dolest ea am inum, nonet lautatur? Orrum reperes tibuscime sunte sit facearchil im ist ea sinctemporem is doluptatem in plab int, odionsequam num hillo te ex et ipiendita conseque nimoloris est alitibera aspersp edignimus dero eatiore molupta parum quaeri acipiendis eum digendus et iusda que pedis inciist, ut eaquibusa dolut in pa volupta quidescimpor si volorem ra voluptat.
-							Optae nos pro cupta volupta dolores eriatus ideliae etur, esti quatis vend</p>
+							<p class="etud" > 	
+												Quelles sont les qualités indispensable pour être développeur ?
+												<br /><br />
+                                                La curiosité, la créativité, la persévérance et énormément de logique. 
+											    Mais il faut surtout avoir conscience des besoins du client, c’est à dire trouver la meilleure 
+											    solution pour un projet tout en respectant les délais.
+												<br /><br />
+												Comment voyez-vous ce métier dans quelques années ?
+												<br /><br />
+												A mon avis le métier de développeur va se démocratiser, il y a besoin de développeurs dans tout 
+												les secteurs d’activité. Tout comme le “webmaster” des années 2000 qui faisait à lui tout seul design, 
+												ergonomie, développement et intégration, et qui s’est transformé en autant de métiers différents, le développeur 
+												web va sûrement se spécialiser à l’avenir.</p>
 						</div>
 					</div>
 				</div>
 			</div>	
-		</div>
+		</div>-->
 	
 	<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
 		<span class="carousel-control-prev-icon" aria-hidden="true"></span>
