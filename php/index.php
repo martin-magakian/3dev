bf9a8f33061a8f36daec1c4b7483329fe1d9d862
leoduchateau <32895315+leoduchateau@users.noreply.github.com>
2017-10-25 10:37:49 +0200

    plan ok

--- php/index.php
+++ php/index.php
@@ -8,6 +8,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
 	<script src="jquery.vide.js"></script>
 	<link rel="stylesheet" href="style.css" >
+	<script src="script.js"></script
 </head>
 <body>
 
